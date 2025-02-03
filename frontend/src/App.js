8221586425517c057afbe88e003be7cc4770a0ac
KUCHHALTUSHAR <kuchhaltushar2@gmail.com>
2025-02-03 23:14:30 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Components/Navbar'; // Make sure to import the Navbar
+import { ClerkProvider, SignedIn, SignedOut, SignIn, SignUp } from '@clerk/clerk-react';
+import Navbar from './Components/Navbar'; // Ensure Navbar is correctly imported
 import Login from './Pages/Login/Login';
+
+const clerkPubKey = "pk_test_bXVzaWNhbC1hbW9lYmEtOTIuY2xlcmsuYWNjb3VudHMuZGV2JA"; // Replace with your actual Clerk public key
 
 const App = () => {
   return (
-    <Router>
-      {/* Include Navbar so it's available on all pages */}
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-    </Router>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <Router>
+        <Navbar /> {/* Navbar will be present on all pages */}
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </ClerkProvider>
   );
 };
 
