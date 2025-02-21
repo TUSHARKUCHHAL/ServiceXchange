5ca374a74cb8258f692fa336ea505fa464128a49
vanshiraj <rajputvasnhika852@gmail.com>
2025-02-21 23:43:57 +0530

    Analytics

--- frontend/src/Pages/NGODashboard/Sidebar.js
+++ frontend/src/Pages/NGODashboard/Sidebar.js
@@ -1,5 +1,6 @@
-// Sidebar.js
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link for navigation
+import Analytics from "../Analytics/Analytics";
 import "./Sidebar.css";
 
 export default function Sidebar() {
@@ -7,11 +8,11 @@ export default function Sidebar() {
     <div className="sidebar">
       <h2>NGO Dashboard</h2>
       <ul>
-        <li>Dashboard</li>
-        <li>Analytics</li>
-        <li>Task List</li>
-        <li>Tracking</li>
-        <li>Settings</li>
+        <li><Link to="/">Dashboard</Link></li>
+        <li><Link to="/analytics">Analytics</Link></li> {/* This opens the Analytics page */}
+        <li><Link to="/tasks">Task List</Link></li>
+        <li><Link to="/tracking">Tracking</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
       </ul>
     </div>
   );
