16451ab933f6571c1ea955aa932dc664768b266d
TUSHARKUCHHAL <kuchhaltushar2@gmail.com>
2025-03-25 21:29:08 +0530

    login

---
+++ backend/models/sendOTP.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// Configure the email transporter
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendOtpEmail = async (email, otp) => {
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Your OTP Code",
+      text: `Your OTP code is: ${otp}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log(`OTP sent to ${email}`);
+  } catch (error) {
+    console.error("Error sending OTP email:", error);
+  }
+};
+
+module.exports = sendOtpEmail;
